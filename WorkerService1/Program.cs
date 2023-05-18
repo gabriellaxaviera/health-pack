82565b6cd6e04374048ca49506680d28030e0bfa
gabriellaxarruda <axavierga@gmail.com>
2023-05-18 11:00:28 -0300

    removing wrong files

--- WorkerService1/Program.cs
+++
@@ -1,8 +0,0 @@
-using health_calc_pack_dotnet;
-
-var imc = new IMC();
-
-var result = imc.Calc(1.67, 86.0);
-var classificacao = imc.GetIMCCategory(result);
-
-Console.WriteLine($"Seu IMC é: {result} e o resultado é: {classificacao}");
