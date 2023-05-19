629fec7e7e0a2e079ade9956a9fc37ad7dbabc3d
gabriellaxarruda <axavierga@gmail.com>
2023-05-19 17:42:02 -0300

    add context to set strategy and add unit test

---
+++ health-pack-dotnet-package/health-calc-pack-dotnet/MacroNutrientesContext.cs
@@ -0,0 +1,21 @@
+﻿
+using health_calc_pack_dotnet.Interfaces;
+using health_calc_pack_dotnet.Model;
+
+namespace health_calc_pack_dotnet
+{
+    public class MacroNutrientesContext
+    {
+        private IMacroNutrientesStrategy? strategy;
+
+        public void SetStrategy (IMacroNutrientesStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public MacroNutrientesModel ExecuteStrategy(double Peso)
+        {
+            return strategy!.CalculateMacroNutrientes(Peso);
+        }
+    }
+}
--- health-pack-dotnet-package/health-dotnet-test/PackageUnitTest/MacroNutrientesUnitTest.cs
+++ health-pack-dotnet-package/health-dotnet-test/PackageUnitTest/MacroNutrientesUnitTest.cs
@@ -1,6 +1,8 @@
 ﻿using health_calc_pack_dotnet.Model;
 using health_calc_pack_dotnet;
 using health_calc_pack_dotnet.Enum;
+using health_calc_pack_dotnet.Interfaces;
+using health_calc_pack_dotnet.Strategy;
 
 namespace health_calc_pack_dotnet_test.PackageTest
 {
@@ -26,6 +28,47 @@ namespace health_calc_pack_dotnet_test.PackageTest
             var result = macroNutrientes.CalculateMacroNutrientes(ObjetivoFisico, Peso);
 
             Assert.Equal(macroNutrientes.getResultForMacros(Expected), macroNutrientes.getResultForMacros(result));
+        }
+
+        [Theory]
+        [InlineData(ObjetivoFisico.PerderPeso, 258, 258, 344)]
+        [InlineData(ObjetivoFisico.ManterPeso, 344, 172, 344)]
+        [InlineData(ObjetivoFisico.GanharPeso, 344, 86, 172)]
+        public void CalculaMacroNutrientes_QuandoDadosValidos_EntaoRetornaMacronutrientesStrategy(ObjetivoFisico ObjetivoFisico,
+            double carboidratos, double gorduras, double proteinas)
+        {
+            //Arrange
+            MacroNutrientesContext strategy = new MacroNutrientesContext();
+            double Peso = 86;
+
+            switch (ObjetivoFisico)
+            {
+                case ObjetivoFisico.GanharPeso:
+                    strategy.SetStrategy(new GanharPesoStrategy());
+                    break;
+                case ObjetivoFisico.PerderPeso:
+                    strategy.SetStrategy(new PerderPesoStrategy());
+                    break;
+                case ObjetivoFisico.ManterPeso:
+                    strategy.SetStrategy(new ManterPesoStrategy());
+                    break;
+                default:
+                    break;
+            }
+
+
+            MacroNutrientesModel Expected = new ()
+            {
+                Carboidratos = carboidratos,
+                Gorduras = gorduras,
+                Proteinas = proteinas,
+            };
+
+            var result = strategy.ExecuteStrategy(Peso);
+
+            Assert.Equal(Expected.Proteinas, result.Proteinas);
+            Assert.Equal(Expected.Gorduras, result.Gorduras);
+            Assert.Equal(Expected.Carboidratos, result.Carboidratos);
         }
     }
 }
